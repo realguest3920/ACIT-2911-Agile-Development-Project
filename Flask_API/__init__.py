ef6429995845ee17464c9171bf47f9ac5ff4c556
realguest3920 <jGondosiswanto@gmail.com>
2026-05-06 11:14:35 -0700

    Added index.html referencing

--- Flask_API/__init__.py
+++ Flask_API/__init__.py
@@ -1,8 +1,17 @@
-from flask import Flask
+import pymongo
+from flask import Flask, render_template
 from .marketplace import api_bp
 
+MongoDBURL = "mongodb+srv://realguest_db_user:eHr283CHww8nLCwG@cluster0.qivfiwd.mongodb.net/"
+maindb = pymongo.MongoClient(MongoDBURL)["AgileDevelopment"]
+
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../templates")
     app.register_blueprint(api_bp)
+
+    @app.route("/")
+    def index() :
+        listings = list(maindb["Listings"].find())
+        return render_template("index.html", listings=listings)
 
     return app

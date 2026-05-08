4f570dc6e5e625362b5b2a1cc0d32f5279a63812
Anthony Cao <anthonycao560@gmail.com>
2026-05-08 10:39:07 -0700

    email verifiy

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,11 @@ from flask import render_template, flash, redirect, url_for
 from flask_login import login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
+mail = Mail(app)
 
 from app import app, mongo
 from app.forms import LoginForm, RegisterForm
@@ -53,12 +58,32 @@ def register():
                 "username": normalize,
                 "email": normemail,
                 "password_hash": hashed_password,
-                "created_at": datetime.now().strftime('%c')
+                "created_at": datetime.now().strftime('%c'),
+                "confirmed": False
             })
             flash('Account created successfully! Please log in.')
+            token = s.dumps(normemail, salt='email-confirm')
+         
+            msg = Message('Confirm Email', sender='agilemarketplace882@gmail.com',recipients=[normemail])
+
+            link = url_for('confirm_email', token=token, _external = True )
+            msg.body= 'Link for confirm {}'.format(link)
+            mail.send(msg)
             return redirect(url_for('login'))
 
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/confirm_email/<token>')
+def confirm_email(token):
+    try:
+        email = s.loads(token, salt='email-confirm', max_age=1000)
+    except SignatureExpired:
+        return "verifaction expired", 400
+    
+    flash("Email confirmed")
+    mongo.db.users.update_one({"email": email}, {"$set": {"confirmed": True}})
+    return "successfully verified email"
+
 
 @app.route('/logout')
 def logout():

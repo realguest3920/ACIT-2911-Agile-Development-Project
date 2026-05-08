8a30aadd92150fc8d060ac03fb06bee371e39a8f
David Ekk <davidmekk@gmail.com>
2026-05-08 10:21:05 -0700

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'guess_the_key'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://Anthony:Ri4NJgC1t3re97Dd@cluster0.qivfiwd.mongodb.net/AgileDevelopment'
